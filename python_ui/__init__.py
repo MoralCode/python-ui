16bea8834c8777bf1fcfb5413a89c5193a03ae68
Adrian Edwards <adrian@adriancedwards.com>
2025-01-20 18:40:05 -0500

    refactor into Terminal class

--- python_ui/__init__.py
+++ python_ui/__init__.py
@@ -1,32 +1,35 @@
 
-def interactive_user_boolean_choice(prompt, default=True, add_directions=True, require_explicit_answer=False):
+class Terminal:
 
-    # TODO: possiby refactor the user interaction into a library that
-    # abstracts things and allows the choice to be made via CSV file,
-    # interactive CLI, or web interface
+    @staticmethod    
+    def boolean_choice(prompt, default=True, add_directions=True, require_explicit_answer=False):
 
-    positive_answer = "Y" if default is not None and default is True else "y"
-    negative_answer = "N" if default is not None and default is False else "n"
+        # TODO: possiby refactor the user interaction into a library that
+        # abstracts things and allows the choice to be made via CSV file,
+        # interactive CLI, or web interface
 
-    prompt_ending = f" ({positive_answer}/{negative_answer}): "
+        positive_answer = "Y" if default is not None and default is True else "y"
+        negative_answer = "N" if default is not None and default is False else "n"
 
-    if add_directions:
-        prompt += prompt_ending
+        prompt_ending = f" ({positive_answer}/{negative_answer}): "
 
-    answer = None
-    while answer is None:
-        response = input(prompt)
+        if add_directions:
+            prompt += prompt_ending
 
-        response = response.lower()
-        if response == "":
-            answer = default
-        elif response == "y":
-            answer = True
-        elif response == "n":
-            answer = False
-        
-        if require_explicit_answer and answer is None:
-            print("A response is required.")
+        answer = None
+        while answer is None:
+            response = input(prompt)
 
-        if answer is not None:
-            return answer
+            response = response.lower()
+            if response == "":
+                answer = default
+            elif response == "y":
+                answer = True
+            elif response == "n":
+                answer = False
+            
+            if require_explicit_answer and answer is None:
+                print("A response is required.")
+
+            if answer is not None:
+                return answer
